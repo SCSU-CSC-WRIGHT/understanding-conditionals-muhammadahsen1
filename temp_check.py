3dc7678b895a93fe0ecdae8b978993a15d5dd5d1
muhammadahsen1 <160660355+muhammadahsen1@users.noreply.github.com>
2024-10-03 17:16:51 -0400

    Create temp_check.py
    Temprature check.

---
+++ temp_check.py
@@ -0,0 +1,13 @@
+# Ask the user for the current temperature in Celsius 
+
+temperature = float(input("Please enter the current temperature in Celsius: ")) 
+
+# Check if the temperature is above 30 degrees 
+
+if temperature > 30: 
+
+    print("Its a hot day.") 
+
+else: 
+
+    print("The weather is cool.")
