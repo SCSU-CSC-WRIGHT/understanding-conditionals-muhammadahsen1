9258efc605b48aa24425ca7a6fd9628f192fff0b
muhammadahsen1 <160660355+muhammadahsen1@users.noreply.github.com>
2024-10-03 17:25:27 -0400

    Create number_comparison.py
     Number comparison

---
+++ number_comparison.py
@@ -0,0 +1,19 @@
+# Ask the user to input two numbers 
+
+num1 = float(input("Enter the first number: ")) 
+
+num2 = float(input ("Enter the second number: ")) 
+
+# Compare the two numbers 
+
+if num1 > num2: 
+
+    print(f"The Larger number is: {num1}") 
+
+elif num1 < num2: 
+
+    print(f"The larger number is: {num2}") 
+
+else: 
+
+    print("Both numbers are equal.")
